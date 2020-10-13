7c535a3ca7956654e1eaef1bce19e15a0ff3a1c8
Bence <sifo996@gmail.com>
2020-10-13 16:12:43 +0200

    Single task execution works

---
+++ ParallelCalculationExamProject/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace ParallelCalculationExamProject
+{
+    public class Calculator
+    {
+        public long calculateNumberOfDivisible(long first, long last, long divisor)
+        {
+            long amountOfNumber = 0;
+            for (long i = first; i <= last; i++)
+            {
+                if (i % divisor == 0)
+                {
+                    amountOfNumber++;
+                }
+            }
+            return amountOfNumber;
+        }
+    }
+}
---
+++ ParallelCalculationExamProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelCalculationExamProject
+{
+    class Program 
+    {
+        private static readonly long MaxNumber = 1_000_000_000L;
+        private static readonly long Divisor = 3;
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Stopwatch sw = new Stopwatch();
+
+            Console.WriteLine("Sequential execution starting...");
+            sw.Start();
+            long result = calculator.calculateNumberOfDivisible(0, MaxNumber, Divisor);
+            sw.Stop();
+            Console.WriteLine("Result          : " + result + " calculated under " + sw.ElapsedMilliseconds + " milliseconds");
+
+        }
+    }
+}
