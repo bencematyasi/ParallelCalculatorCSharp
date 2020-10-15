2a22d555a10d4e61dc78b5b9211e21b67f156c71
Bence <sifo996@gmail.com>
2020-10-15 14:28:39 +0200

    ForEaches replaced with Parallel one, cleanup and code separation

--- ParallelCalculationExamProject/Program.cs
+++ ParallelCalculationExamProject/Program.cs
@@ -15,48 +15,68 @@ namespace ParallelCalculationExamProject
         {
             Calculator calculator = new Calculator();
             Stopwatch sw = new Stopwatch();
+
             Console.WriteLine("Sequential execution is starting...");
-            sw.Start();
-            long result = calculator.calculateNumberOfDivisible(0, MaxNumber, Divisor);
-            sw.Stop();
-            Console.WriteLine("Result          : " + result + " calculated under " + sw.ElapsedMilliseconds + " milliseconds");
-
-            List<Task<long>> tasklist = new List<Task<long>>();
-
-            CustomTaskScheduler taskScheduler = new CustomTaskScheduler(Environment.ProcessorCount);
-            long taskResult = 0;
+            SequentialRun(calculator, sw);
+            Console.WriteLine("----------------------------------------");
             Console.WriteLine("Parallel execution is starting...");
+            ParallelRun(calculator, sw);
 
+
+        }
+
+        public static void SequentialRun(Calculator calculator, Stopwatch sw)
+        {
+            sw.Reset();
+            sw.Start();
+            long result = calculator.calculateNumberOfDivisible(1, MaxNumber, Divisor);
+            sw.Stop();
+            Console.WriteLine("Result: " + result + " calculated under {0:F5} sec.", (sw.ElapsedMilliseconds / 1000f));
+        }
+
+        public static void ParallelRun(Calculator calculator, Stopwatch sw)
+        {
+            //CustomTaskScheduler taskScheduler = new CustomTaskScheduler(Environment.ProcessorCount);
+            sw.Reset();
+            sw.Start();
             CancellationTokenSource cts = new CancellationTokenSource();
+            cts.CancelAfter(5000);
+            long taskResult = 0;
+            List<Task<long>> taskList = new List<Task<long>>();
 
-            TaskFactory tf = new TaskFactory(taskScheduler);
-            sw.Restart();
             for (int i = 1; i < Environment.ProcessorCount + 1; i++)
             {
-                var last = MaxNumber / (Environment.ProcessorCount);
-                var first = ((last * i) - last) + 1;
-                if (i == 1)
-                {
-                    first = 0;
-                }
-                last = last * i;
+                var fraction = MaxNumber / (Environment.ProcessorCount);
+                var first = ((fraction * i) - fraction) + 1;
+                var last = fraction * i;
+
+                //Creating as many Tasks as thread is counted in the machine, and setting each task a CancellationToken, that will cancel the task after 5 seconds of runtime 
                 Task<long> task = new Task<long>(() => calculator.calculateNumberOfDivisible(first, last, Divisor), cts.Token);
-                tasklist.Add(task);
+                //Add the created task to taskList
+                taskList.Add(task);
             }
-            //TODO get task result and add them together.
-            foreach (var task in tasklist)
+
+            /*Start each task.
+            The order of execution does not matter*/
+            Parallel.ForEach(taskList.ToArray(), task =>
             {
-                task.Start(taskScheduler);
-            }
-            Task.WaitAll(tasklist.ToArray());
-            foreach (var t in tasklist)
+                task.Start();
+            });
+
+            //Wait all the tasks to be completed
+            Task.WaitAll(taskList.ToArray());
+
+            /*Add the tasks' return value/result to the taskResult variable.
+            The order of addition does not matter, but to receive all the results the Interlocked is required to have*/
+            Parallel.ForEach(taskList.ToArray(), task =>
             {
-                taskResult += t.Result;
-            }
+                Interlocked.Add(ref taskResult, task.Result);
+            });
             cts.Dispose();
             sw.Stop();
 
-            Console.WriteLine("Result          : " + taskResult + " calculated under " + sw.ElapsedMilliseconds + " milliseconds");
+            Console.WriteLine("Result: " + taskResult + " calculated under {0:F5} sec.", (sw.ElapsedMilliseconds / 1000f));
+
         }
     }
 }
